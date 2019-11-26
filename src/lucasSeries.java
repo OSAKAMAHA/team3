9a7240637796a860350e8246371ceab92a4d7ce9
OSAKAMAHA <osamaakhader@gmail.com>
2019-11-26 22:06:22 +0200

    final final

--- src/lucasSeries.java
+++ src/lucasSeries.java
@@ -1,25 +1,25 @@
-public abstract class lucasSeries extends Thread implements ISubscriber{
-    
+public class lucasSeries extends Thread implements ISubscriber{
+
  private int n;
-    public int lucas(int n) 
-    { 
+    public int lucas(int n)
+    {
 
-        if (n == 0) 
-            return 2; 
-        if (n == 1) 
-            return 1; 
+        if (n == 0)
+            return 2;
+        if (n == 1)
+            return 1;
 
 
-        return lucas(n - 1)  + lucas(n - 2); 
-       
-        
+        return lucas(n - 1)  + lucas(n - 2);
+
+
     }
 
-public void run(){ 
-     system.out.println("lucas "+ lucas(n));
+public void run(){
+     System.out.println("lucas "+ lucas(n));
 }
-    public void notifySubscriber(Topic topic){ 
-        n=(int)topic.getvalue();
+    public void notifySubscriber(Topic topic){
+        n=(int)topic.getValue();
         this.start();
     }
 
